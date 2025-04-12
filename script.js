042b9d64e9187f7623745e0c9cf17f98a3c6976a
Ankit Kumar <jiraankit@gmail.com>
2025-04-12 17:09:16 +0530

    Adding New Pages

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ document.addEventListener("DOMContentLoaded", function () {
         navLinks.classList.toggle("active");
     });
 });
+
+
+// for Carousel part
+
+document.addEventListener('DOMContentLoaded', () => {
+    const carousels = document.querySelectorAll('.carousel');
+  
+    carousels.forEach((carousel) => {
+      const images = carousel.querySelectorAll('.carousel-img');
+      let index = 0;
+  
+      const showImage = (i) => {
+        images.forEach((img, idx) => {
+          img.classList.toggle('active', idx === i);
+        });
+      };
+  
+      // Initial image
+      showImage(index);
+  
+      // Auto-slide every 3 seconds
+      setInterval(() => {
+        index = (index + 1) % images.length;
+        showImage(index);
+      }, 3000); // change every 3 seconds
+    });
+  });
+  
